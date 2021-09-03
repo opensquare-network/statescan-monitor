2ee34a1fc1710980c972f5da2ab93b224df685f4
Yongfeng LI <wliyongfeng@gmail.com>
2021-09-03 14:15:43 +0800

    Revert "Update"
    This reverts commit 923c14ebc804717f20319393d9ad0ad08ed8efd5.

--- src/index.js
+++ src/index.js
@@ -10,19 +10,16 @@ async function main() {
   }
 
   console.log(`Begin to restart mongod and docker containers`);
-  await new Promise((resolve, reject) => {
-    exec("systemctl restart mongod", (err, stdOut) => {
-      if (err) {
-        console.error("error restart mongod")
-        reject()
-        return
-      }
+  exec("systemctl restart mongod", (err, stdOut) => {
+    if (err) {
+      console.error("error for ls")
+      return
+    }
 
-      exec('docker restart $(docker ps -q)', (err, stdOut) => {
-        resolve()
-        console.log('stdOut', stdOut)
-      })
+    exec('docker restart $(docker ps -q)', (err, stdOut) => {
+      console.log('stdOut', stdOut)
     })
+
   })
 }
 
