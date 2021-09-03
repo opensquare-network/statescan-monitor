35a1bcb612db7d164f5b83c2cbe640b0dac89948
Yongfeng LI <wliyongfeng@gmail.com>
2021-09-03 14:09:19 +0800

    Update

--- src/index.js
+++ src/index.js
@@ -2,57 +2,6 @@ const osu = require("node-os-utils")
 const mem = osu.mem;
 const { exec } = require("child_process");
 
-async function lsa() {
-  return new Promise(((resolve, reject) => {
-    exec("ls -la", (err, stdout, stderr) => {
-      if (err) {
-        reject(err)
-      } else if (stderr) {
-        reject(stderr)
-      } else if (stdout) {
-        console.log(stdout)
-        resolve(stdout)
-      } else {
-        reject()
-      }
-    })
-  }))
-}
-
-async function restartMongod() {
-  return new Promise((resolve, reject) => {
-    exec("systemctl restart mongod", (err, stdout, stderr) => {
-      if (err) {
-        reject(err)
-      } else if (stderr) {
-        reject(stderr)
-      } else if (stdout) {
-        console.log(stdout)
-        resolve(stdout)
-      } else {
-        reject()
-      }
-    })
-  })
-}
-
-async function restartDockerContainers() {
-  return new Promise((resolve, reject) => {
-    exec("docker restart $(docker ps -q)", (err, stdout, stderr) => {
-      if (err) {
-        reject(err)
-      } else if (stderr) {
-        reject(stderr)
-      } else if (stdout) {
-        console.log(stdout)
-        resolve(stdout)
-      } else {
-        reject()
-      }
-    })
-  })
-}
-
 async function main() {
   const memInfo = await mem.info();
   if (memInfo.freeMemMb > 1000) {
@@ -61,8 +10,17 @@ async function main() {
   }
 
   console.log(`Begin to restart mongod and docker containers`);
-  await restartMongod();
-  await restartDockerContainers();
+  exec("systemctl restart mongod", (err, stdOut) => {
+    if (err) {
+      console.error("error for ls")
+      return
+    }
+
+    exec('docker restart $(docker ps -q)', (err, stdOut) => {
+      console.log('stdOut', stdOut)
+    })
+
+  })
 }
 
 main().catch(e => console.error(e))
