4a2d71510598b8b142464849b3d201fc7e137423
qiyisi <qiyisi@qq.com>
2022-01-05 17:33:02 +0800

    Update pm2 restart

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-const dotenv = require("dotenv")
+const dotenv = require("dotenv");
+const pm2 = require("pm2");
 dotenv.config();
 
 const { getScanFinalizedHeight } = require("./scan");
 const { getFinalizedNumber } = require("./chain/height");
-const { exec } = require("child_process");
 const { disconnect } = require("./chain/api");
 
 async function main() {
@@ -11,17 +11,33 @@ async function main() {
   const scanHeight = await getScanFinalizedHeight();
 
   if (head - scanHeight < 30) {
-    console.log(`No problem, finalized height: ${ head }, scan height: ${ scanHeight }`)
+    console.log(
+      `No problem, finalized height: ${head}, scan height: ${scanHeight}`
+    );
     await disconnect();
     process.exit(0);
-    return
+    return;
   }
 
-  const commands = process.env.COMMANDS;
-  console.log(`Begin to execute commands ${ commands }`);
-  exec(commands, (err, stdOut) => {
-    console.log('stdOut', stdOut)
-  })
+  const names = (process.env.NAMES || "").split(";");
+  console.log("Begin to restart pm2");
+  pm2.connect((err) => {
+    if (err) {
+      console.log(err);
+      process.exit(2);
+    }
+
+    pm2.list((err, list) => {
+      (list || []).forEach((item) => {
+        if (names.includes(item.name)) {
+          pm2.restart(item.name, (err, proc) => {
+            console.log(err);
+            pm2.disconnect();
+          });
+        }
+      });
+    });
+  });
 }
 
-main().catch(e => console.error(e))
+main().catch((e) => console.error(e));
